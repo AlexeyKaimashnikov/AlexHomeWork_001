f4dbb935cdb7ec9f83c3b886d52b77741cb7ef59
Alexei <alexej484@gmail.com>
2022-09-04 22:26:19 +0600

    Task_2

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿public class primer
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Enter the number:");
+        string x = Console.ReadLine ();
+        int a = Convert.ToInt32 (x);
+        Console.WriteLine ("Enter the second number:");
+        string y = Console.ReadLine ();
+        int b = Convert.ToInt32 (y);
+        if (a>b)
+        {
+            Console.WriteLine ("a = "+a+"; b = "+b+"; max = "+a);
+        }
+        else
+            {
+            Console.WriteLine ("a = "+a+"; b = "+b+"; max = "+b);
+            }
+        }
+}
---
+++ Task_2/Program.cs
@@ -0,0 +1,27 @@
+﻿public class primer
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Enter the number:");
+        string x = Console.ReadLine ();
+        int a = Convert.ToInt32 (x);
+        Console.WriteLine ("Enter the second number:");
+        string y = Console.ReadLine ();
+        int b = Convert.ToInt32 (y);
+        Console.WriteLine ("Enter the third number:");
+        string z = Console.ReadLine ();
+        int c = Convert.ToInt32 (z);
+         if (a>b && a>c)
+        {
+            Console.WriteLine (a+", "+b+", "+c+" -> "+a);
+        }
+        if (b>a && b>c)
+        {
+            Console.WriteLine (a+", "+b+", "+c+" -> "+b);
+        }
+        if (c>a && c>b)
+        {
+            Console.WriteLine (a+", "+b+", "+c+" -> "+c);
+        }
+    }
+}
