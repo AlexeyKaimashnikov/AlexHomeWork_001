4958371d435f80a5854c93ffe4f1eeded67a1289
Alexei <alexej484@gmail.com>
2022-09-05 17:25:09 +0600

    Task 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,18 @@
+﻿public class primer
+{
+    public static void Main()
+        {
+            Console.WriteLine ("Enter the number:");
+        string x = Console.ReadLine ();
+        int a = Convert.ToInt32 (x);
+            
+            if (a % 2 == 0)
+                {
+                  Console.WriteLine(a+" -> yes");
+                }
+            else 
+                 { 
+                  Console.WriteLine(a+" -> no");
+                 }
+        }
+}
